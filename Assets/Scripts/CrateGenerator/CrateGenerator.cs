52e5f1192dbb1356ef5f375ad4ad5a5417e4b934
XenoDaimon <theo.touzet@epitech.eu>
2016-12-10 18:48:42 +0100

    Conflict OK + Changed background

--- Assets/Scripts/CrateGenerator/CrateGenerator.cs
+++ Assets/Scripts/CrateGenerator/CrateGenerator.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrateGenerator : MonoBehaviour {
-	public GameObject crate = null;
+	public GameObject []crates;
 	public float spawnIntervalMax = 5.0f;
 	public float spawnIntervalMin = 2.0f;
 	private float nextSpawn;
 
 	void Start () {
-		if (crate == null)
-			crate = (GameObject)Resources.Load("Prefabs/Barrel", typeof(GameObject));
 		nextSpawn = Random.Range (spawnIntervalMin, spawnIntervalMax);
 	}
 
 	void Update () {
 		if (nextSpawn <= 0) {
-			GameObject obj = Instantiate (crate);
+			GameObject obj = Instantiate (crates[Random.Range(0, crates.Length)]);
 			obj.transform.SetParent (this.transform);
 			obj.transform.position = this.transform.position;
 			nextSpawn = Random.Range(spawnIntervalMin, spawnIntervalMax);
