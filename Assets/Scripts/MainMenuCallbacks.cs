5b83d3d728101b0c8dae476202cf331830985e27
GasparQ <quentin.gasparotto@epitech.eu>
2016-12-10 13:18:23 +0100

    Début du main menu
    - ajout des bouttons
    - ajout de la police pirate
    - ajout du script avec callbacks pour chaque button

---
+++ Assets/Scripts/MainMenuCallbacks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCallbacks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickPlay()
+    {
+
+    }
+
+    public void OnClickControls()
+    {
+
+    }
+
+    public void OnClickHighscores()
+    {
+
+    }
+
+    public void OnClickCredits()
+    {
+
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
