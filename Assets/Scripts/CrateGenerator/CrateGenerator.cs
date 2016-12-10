719af628b0fc7441b6f4ec0920ff2eaa88fc8a2d
XenoDaimon <theo.touzet@epitech.eu>
2016-12-10 14:33:28 +0100

    Adding prefabs + adding generator scripts

---
+++ Assets/Scripts/CrateGenerator/CrateGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateGenerator : MonoBehaviour {
+	public GameObject crate = null;
+	public float spawnIntervalMax = 10.0f;
+	public float spawnIntervalMin = 5.0f;
+	private float nextSpawn;
+
+	void Start () {
+		if (crate == null)
+			crate = (GameObject)Resources.Load("Prefabs/Barrel", typeof(GameObject));
+		nextSpawn = Random.Range (spawnIntervalMin, spawnIntervalMax);
+	}
+
+	void Update () {
+		if (nextSpawn <= 0) {
+			GameObject obj = Instantiate (crate);
+			obj.transform.SetParent (this.transform);
+			obj.transform.position = this.transform.position;
+			nextSpawn = Random.Range(spawnIntervalMin, spawnIntervalMax);
+		}
+		nextSpawn -= Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/CrateGenerator/MenuButtonGenerate.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuButtonGenerate : MonoBehaviour {
+	private static GameObject createObjectGenerator(Transform prt)
+	{
+		GameObject obj = new GameObject ();
+		obj.name = "Generator";
+		obj.transform.SetParent (prt);
+		obj.AddComponent<CrateGenerator> ();
+		return (obj);
+	}
+
+	private static GameObject checkGenerators()
+	{
+		GameObject generators = GameObject.Find ("Generators");
+		if (generators == null) {
+			generators = new GameObject ();
+			generators.name = "Generators";
+		}
+		return (generators);
+	}
+
+	[MenuItem("Generator/Add/Add barrel generator %&gb", false, 1)]
+	private static void addBarrelGenerator()
+	{
+		GameObject generators = checkGenerators ();
+		GameObject gen = createObjectGenerator(generators.transform);
+		gen.GetComponent<CrateGenerator> ().crate = (GameObject)Resources.Load ("Prefabs/Barrel", typeof(GameObject));
+		gen.name = "Barrel generator";
+	}
+
+	[MenuItem("Generator/Add/Add crate generator %&gc", false, 2)]
+	private static void addCrateGenerator()
+	{
+		GameObject generators = checkGenerators ();
+		GameObject gen = createObjectGenerator (generators.transform);
+		gen.GetComponent<CrateGenerator> ().crate = (GameObject)Resources.Load ("Prefabs/Crate", typeof(GameObject));
+		gen.name = "Crate generator";
+	}
+
+	[MenuItem("Generator/Add/Add selected object generator %&gs", false, 3)]
+	private static void addSelectedObjectGenerator()
+	{
+		GameObject generators = checkGenerators ();
+		GameObject gen = createObjectGenerator (generators.transform);
+		gen.GetComponent<CrateGenerator> ().crate = (GameObject)Resources.Load ("Prefabs/" + Selection.activeGameObject.name, typeof(GameObject));
+		gen.name = Selection.activeGameObject.name + " generator";
+	}
+
+	[MenuItem("Generator/Clear/Reset generators %&#r", false, 4)]
+	private static void resetGenerators()
+	{
+		GameObject generators = GameObject.Find ("Generators");
+		if (generators != null) {
+			while (generators.transform.childCount > 0)
+				GameObject.DestroyImmediate (generators.transform.GetChild (0).gameObject);
+		}
+	}
+}
