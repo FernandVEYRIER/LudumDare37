8f3cc7db3aaef1a0ddb9235040280f41d1a8530a
Fernand <fernand.veyrier@epitech.eu>
2016-12-10 12:37:29 +0100

    Game Manager

---
+++ Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour {
+
+	[SerializeField] private float rotationVel = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameManager.GM.GameState == GameManager.eGameState.PLAY)
+		{
+			transform.Rotate (new Vector3 (0, 0, rotationVel * -Input.GetAxis("Horizontal") * Time.deltaTime));
+		}
+	}
+}
